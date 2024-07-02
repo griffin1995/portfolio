2786fed606ef72e34d2afe4e28f37e50e64134eb
griffin1995 <jtgriffin95@gmail.com>
2024-07-02 19:16:26 +0100

    added serializer

--- api/views.py
+++ api/views.py
@@ -1,16 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Define your views here.
-def main(request):
-    """
-    Handle the request and return a simple HTTP response with the text "TEST".
 
-    Args:
-        request (HttpRequest): The request object used to generate the response.
-
-    Returns:
-        HttpResponse: An HTTP response object containing the text "TEST".
-    """
-    return HttpResponse("TEST")
