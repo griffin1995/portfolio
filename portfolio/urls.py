a44c1cf6d9e5835f20380a754e2f1c342ebcc77d
griffin1995 <jtgriffin95@gmail.com>
2024-07-03 21:12:30 +0100

    added frontend url paths

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # Include URLs from the 'api' application
     path("api/", include("api.urls")),
+    path("", include("frontend.urls")),
 ]
