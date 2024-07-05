1c73cc9c489c029ce5f813ae5c671dd3f953d1e0
griffin1995 <jtgriffin95@gmail.com>
2024-07-05 12:18:34 +0100

    migrations

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,4 +5,9 @@ from .views import index
 urlpatterns = [
     # Route for the root URL, which calls the index view/function
     path("", index),
+    # Route for the "/join" URL, which also calls the index view/function
+    # These routes allow Django to pass control to React for further routing
+    path("join", index),
+    # Route for the "/create" URL, which also calls the index view/function
+    path("create", index),
 ]
